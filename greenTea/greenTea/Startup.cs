d44dd2b1d065fcb40d6c09ea1be29e059eae1aac
Vagner Lopes <72874334+Vagner-Lopes@users.noreply.github.com>
2022-01-06 12:02:54 -0300

    Startup e Migrations
    ## Adicionado contexto a Startup e criado primeira migration  "mig-1"

--- greenTea/greenTea/Startup.cs
+++ greenTea/greenTea/Startup.cs
@@ -1,3 +1,4 @@
+using greenTea.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace greenTea
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Contexto>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
